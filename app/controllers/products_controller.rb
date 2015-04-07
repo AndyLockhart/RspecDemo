751c99c23b86953240473055c86e5fe796d05949
AndyLockhart <andylockhart@AndyLockhartdeMacBook-Pro.local>
2015-04-07 17:42:46 +0800

    迭代7完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -54,10 +54,12 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    @product = Product.find(params[:id])
     @product.destroy
+
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to(products_url) }
+      format.xml  { head :ok }
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,16 @@ class Product < ActiveRecord::Base
   	validates_uniqueness_of :title
    	validates_format_of :image_url, 
    		:with => %r{\.(gif|jpg|png)\z}i,
-		:message => 'must be a URL for GIF, JPG or PNG image.(gif|jpg|png)'
+		:message => 'must be a URL for GIF, JPG or PNG image.(gif|jpg|png)'\
+
+
+    def self.find_products_for_sale
+      all.order(title: :desc)
+    end
 
   	protected
-   		def price_must_be_at_least_a_cent
-			errors.add(:price, 'should be at least 0.01' ) if price.nil? || price < 0.01 
-		end
+
+ 		def price_must_be_at_least_a_cent
+		  errors.add(:price, 'should be at least 0.01' ) if price.nil? || price < 0.01 
+	  end
 end
